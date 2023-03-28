12987ecee4131572a1c0b59ff03b678cdc0cc66d
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-03-28 11:22:07 -0400

    exports the isConnected boolean, conditions state of lists on a new firebase snapshot or from cache

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -8,7 +8,7 @@ import {
 import { collection, addDoc, onSnapshot, query, where } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const ShoppingLists = ({ db, route }) => {
+const ShoppingLists = ({ db, isConnected, route }) => {
   const { userID } = route.params;
 
   const [lists, setLists] = useState([]);
@@ -16,24 +16,37 @@ const ShoppingLists = ({ db, route }) => {
   const [item1, setItem1] = useState("");
   const [item2, setItem2] = useState("");
 
+  //initializes a firestore unsubscribe reference
+  let unsubShoppinglists;
+  
+  // upon mount of the component, checks the value of isConnected, a state variable monitoring status within App.js exported through props,
+  // if connection, generate a snapshot of firestore, restraining the query to the anonymously signed in user, that creates a new array out of firestore collection, 
+  // puts the new array onto the state, and caches the new array. 
+  // if isConnection false, call a function to load state array with cached data.  
   useEffect(() => {
+      if(isConnected){
+        
+        if (unsubShoppinglists) unsubShoppinglists();
+        unsubShoppinglists = null;
 
-    const shoppingListRef = collection(db, "shoppingList")
-    const q = query(shoppingListRef, where("uid", "==", userID));
-    const unsubShoppinglists = onSnapshot(q,(documentsSnapshot) => {
-      let newLists = [];
-      documentsSnapshot.forEach(doc => {
-        newLists.push({ id: doc.id, ...doc.data() })
-      });
-      cacheShoppingList(newLists);
-      setLists(newLists);
-    });
-
-    // Clean up code
-    return () => {
-      if (unsubShoppinglists) unsubShoppinglists();
+        const shoppingListRef = collection(db, "shoppingList")
+        const q = query(shoppingListRef, where("uid", "==", userID));
+        unsubShoppinglists = onSnapshot(q,(documentsSnapshot) => {
+          let newLists = [];
+          documentsSnapshot.forEach(doc => {
+            newLists.push({ id: doc.id, ...doc.data() })
+          });
+          cacheShoppingList(newLists);
+          setLists(newLists);
+        });
+    } else {
+      loadCacheList();
     }
-  }, []);
+    // Clean up code
+      return () => {
+        if (unsubShoppinglists) unsubShoppinglists();
+      }
+  }, [isConnected]); // triggers the useEffect every time the app connection status ( exists on the state via App.js) change.
 
 
   // try to store a list array fetched out of firebase in client storage, return error if AsyncStorage fails.
@@ -46,6 +59,10 @@ const ShoppingLists = ({ db, route }) => {
     }
   }
 
+
+
+// async function responding to a user submission of the text input data, awaits response from firestore of the updated collection, if successful
+// it merges the submitted list with the existing lists on the state and resets state. 
   const addShoppingList = async (newList) => {
     const newListRef = await addDoc(collection(db, "shoppingList"), newList);
     if (newListRef.id) {
@@ -59,6 +76,11 @@ const ShoppingLists = ({ db, route }) => {
     }
   }
 
+  const loadCacheList = async () =>{
+    const cachedLists = await AsyncStorage.getItem('shoppingLists');
+    setLists(JSON.parse(cachedLists));
+  }
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -70,39 +92,44 @@ const ShoppingLists = ({ db, route }) => {
           </View>
         }
       />
+      { (isConnected) ? 
+      
       <View style={styles.listForm}>
-        <TextInput
-          style={styles.listName}
-          placeholder="List Name"
-          value={listName}
-          onChangeText={setListName}
-        />
-        <TextInput
-          style={styles.item}
-          placeholder="Item #1"
-          value={item1}
-          onChangeText={setItem1}
-        />
-        <TextInput
-          style={styles.item}
-          placeholder="Item #2"
-          value={item2}
-          onChangeText={setItem2}
-        />
-        <TouchableOpacity
-          style={styles.addButton}
-          onPress={() => {
-            const newList = {
-              uid: userID,
-              name: listName,
-              items: [item1, item2]
-            }
-            addShoppingList(newList);
-          }}
-        >
-          <Text style={styles.addButtonText}>Add</Text>
-        </TouchableOpacity>
-      </View>
+            <TextInput
+                style={styles.listName}
+                placeholder="List Name"
+                value={listName}
+                onChangeText={setListName}
+              />
+              <TextInput
+                style={styles.item}
+                placeholder="Item #1"
+                value={item1}
+                onChangeText={setItem1}
+              />
+              <TextInput
+                style={styles.item}
+                placeholder="Item #2"
+                value={item2}
+                onChangeText={setItem2}
+              />
+              <TouchableOpacity
+                style={styles.addButton}
+                onPress={() => {
+                  const newList = {
+                    uid: userID,
+                    name: listName,
+                    items: [item1, item2]
+                  }
+                  addShoppingList(newList);
+                }}
+              >
+                <Text style={styles.addButtonText}>Add</Text>
+              </TouchableOpacity>
+        </View> 
+            : 
+          null // render no text input / button UI if there is no network to post the data. 
+        }
       {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
     </View>
   )
